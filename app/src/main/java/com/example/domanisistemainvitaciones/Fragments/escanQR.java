f90a9186788ecce432ce1d7e701bbc85ab720565
David Valenzuela <davidevc.32@gmail.com>
2019-08-18 10:29:03 -0400

    inicio de fragment escan qr, pruebas

--- app/src/main/java/com/example/domanisistemainvitaciones/Fragments/escanQR.java
+++ app/src/main/java/com/example/domanisistemainvitaciones/Fragments/escanQR.java
@@ -1,16 +1,27 @@
 package com.example.domanisistemainvitaciones.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.domanisistemainvitaciones.R;
+import com.example.domanisistemainvitaciones.qrZxing.IntentIntegrator;
+import com.example.domanisistemainvitaciones.qrZxing.IntentResult;
+
+import org.w3c.dom.Text;
 
 
 public class escanQR extends Fragment {
 
+    Button button;
+    TextView txt;
 
     public escanQR() {
 
@@ -19,6 +30,19 @@ public class escanQR extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup  container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_escan_qr, container, false);
+        View view = inflater.inflate(R.layout.fragment_escan_qr, container, false);
+
+        button = (Button) view.findViewById(R.id.button);
+        txt = (TextView) view.findViewById(R.id.textView4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
     }
+
 }
